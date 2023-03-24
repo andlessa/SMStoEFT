02bc74b5b6ae2e6ea227f4ca893b2606e6ec3546
Andre Lessa <lessa.a.p@gmail.com>
2023-03-24 15:51:06 -0300

    Improved form factor (off-shell)

--- modelFiles/gghEFT_UFO_withFormFactor_Fortran/Fortran/functions.f
+++ modelFiles/gghEFT_UFO_withFormFactor_Fortran/Fortran/functions.f
@@ -2,16 +2,20 @@
 
 double complex function myFormFactor(S)
 
-   double precision S ! higgs momentum squared
-   double precision r
+   double complex S ! higgs momentum squared (allows for off-shell Higgs)
+   double complex r,mtop
+   double complex ScalarC0
    include 'input.inc' ! include all external model parameter
    include 'coupl.inc' ! include other parameters
    
+   
    if (MDL_CG > 0.0) then
-       r = SQRT(S)/(2*MDL_MT)
-       myFormFactor = (3./2.)*(1.0/(r**2))*(1. + (1.-1./(r**2))*(ASIN(r)**2))
+       mtop = MDL_MT   
+       ScalarC0 = (CDLOG((2*mtop**2 - S + CDSQRT(S*(-4*mtop**2 + S)))/(2.*mtop**2))**2)/(2.*S) ! Allows for off-shell higgs
+!       ScalarC0 = (-2./S)*ASIN(SQRT(S/(4*mtop**2)))**2 ! Limit for on-shell Higgs
+       myFormFactor = (3./2.)*(2.*mtop/S)*(2*mtop + mtop*(4*mtop**2-S)*ScalarC0)  
    else
-       myFormFactor = 1.0
+       myFormFactor = 1.0 ! Limit for mtop >> S (controlled by sign of cG coupling)
    end if 
    
    return 
