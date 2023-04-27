5a49e3425845b4206c51cfffd51e250bf291272e
Andre Lessa <lessa.a.p@gmail.com>
2023-04-27 18:12:57 -0300

    Added fixed MG5 (to allow for small widths for colored particles)

--- installer.sh
+++ installer.sh
@@ -28,16 +28,18 @@ fi
 
 cd $homeDIR
 
-madgraph="MG5_aMC_v3.4.2.tar.gz"
-URL=https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/$madgraph
+#madgraph="MG5_aMC_v3.4.2.tar.gz"
+#URL=https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/$madgraph
+madgraph="MG5_aMC_v3.4.2_fix.tar.gz"
 echo -n "Install MadGraph (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	mkdir MG5;
-	echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; tar -zxf $madgraph -C MG5 --strip-components 1;
+	#echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; 
+	tar -zxf $madgraph -C MG5 --strip-components 1;
 	cd ./MG5/bin;
 	echo "[installer] installing HepMC under MadGraph5"
-    echo "install hepmc\ninstall lhapdf6\ninstall pythia8\ninstall Delphes\nexit\n" > mad_install.txt;
+        echo "install hepmc\ninstall lhapdf6\ninstall pythia8\ninstall Delphes\nexit\n" > mad_install.txt;
 	./mg5_aMC -f mad_install.txt
 
 	rm mad_install.txt;
