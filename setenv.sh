aaedb05ea2ac841d68d7684d1d4e454a6789c61a
Andre Lessa <lessa.a.p@gmail.com>
2023-10-23 11:45:03 -0300

    Trying Delphes recast

--- setenv.sh
+++ setenv.sh
@@ -8,4 +8,5 @@ lhapdfDIR=$currentDIR/MG5/HEPTools/lhapdf6_py3
 export LD_LIBRARY_PATH=$pythiaDIR/lib:$lhapdfDIR/lib:$LD_LIBRARY_PATH
 #Make sure Delphes can be found by ROOT
 export ROOT_INCLUDE_PATH=$delphesDIR/external
+export PYTHON_PATH=$lhapdfDIR/lib:$PYTHON_PATH
 
