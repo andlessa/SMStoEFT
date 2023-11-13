69f187749095c03327d3e458a3a8e86ba44c0d74
Andre Lessa <lessa.a.p@gmail.com>
2023-11-13 17:44:43 -0300

    Fix for collierfix

--- fixForCollier.sh
+++ fixForCollier.sh
@@ -7,12 +7,15 @@ currentDIR="$( pwd )"
 
 procDIR="$(cd "$(dirname "$1")" >/dev/null; pwd -P)/$(basename "$1")"
 
+for f in ./MG5/HEPTools/collier/COLLIER*; do
+    coolDIR=$f
+done
+
 
 cp auxFiles/SubProcesses/makefile $procDIR/SubProcesses/
 cp auxFiles/Source/makefile $procDIR/Source/
-cp MG5/HEPTools/collier/COLLIER-1.2.8/modules/collier.mod $procDIR/Source/MODEL/
-cp MG5/HEPTools/collier/COLLIER-1.2.8/libcollier.a $procDIR/lib
+cp $coolDIR/modules/collier.mod $procDIR/Source/MODEL/
+cp $coolDIR/libcollier.a $procDIR/lib
 
 echo "Fixes for $procDIR done."
-
 
