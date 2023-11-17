15f5a9bb240ebe622c88f7abf55091570a0eb811
Andre Lessa <lessa.a.p@gmail.com>
2023-11-17 18:02:48 -0300

    Updated plot

--- notebooks/auxPlots.py
+++ notebooks/auxPlots.py
@@ -443,3 +443,55 @@ def Cq(mChi,mST,yDM,gs):
         r = r*mST**2*mChi**4/((mChi**2-mST**2)**4)
     
     return c*r               
+
+def label_line(fig,line, label_text, 
+               near_i=None, near_x=None, near_y=None, 
+               rotation_offset=0, offset=(0,0),fontsize=13,
+               xmin=None,rotation=None,boxalpha=1.0):
+    """call 
+        l, = plt.loglog(x, y)
+        label_line(l, "text", near_x=0.32)
+    """
+    def put_label(i):
+        """put label at given index"""
+        i = min(i, len(x)-2)
+        dx = sx[i+1] - sx[i]
+        dy = sy[i+1] - sy[i]
+        if rotation is None:
+            rot = np.rad2deg(np.arctan2(dy, dx)) + rotation_offset
+        else:
+            rot = rotation
+        pos = [(x[i] + x[i+1])/2. + offset[0], (y[i] + y[i+1])/2 + offset[1]]
+        if pos[0] > xmin:
+            plt.text(pos[0], pos[1], label_text, size=fontsize, 
+                     rotation=rot, color = line.get_color(),
+                     ha="center", va="center", bbox = dict(ec='1',fc='1',alpha=boxalpha))
+
+    x = line.get_xdata()
+    y = line.get_ydata()
+    ax = fig.get_axes()[0]
+    if ax.get_xscale() == 'log':
+        sx = np.log10(x)    # screen space
+    else:
+        sx = x
+    if ax.get_yscale() == 'log':
+        sy = np.log10(y)
+    else:
+        sy = y
+
+    # find index
+    if near_i is not None:
+        i = near_i
+        if i < 0: # sanitize negative i
+            i = len(x) + i
+        put_label(i)
+    elif near_x is not None:
+        for i in range(len(x)-2):
+            if (x[i] < near_x and x[i+1] >= near_x) or (x[i+1] < near_x and x[i] >= near_x):
+                put_label(i)
+    elif near_y is not None:
+        for i in range(len(y)-2):
+            if (y[i] < near_y and y[i+1] >= near_y) or (y[i+1] < near_y and y[i] >= near_y):
+                put_label(i)
+    else:
+        raise ValueError("Need one of near_i, near_x, near_y")
