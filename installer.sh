f9a2d2b20710f1d6ad09a83191c98d99033f5843
Andre Lessa <lessa.a.p@gmail.com>
2024-06-13 16:06:56 -0300

    Small fix in installer

--- installer.sh
+++ installer.sh
@@ -34,14 +34,14 @@ fi
 
 cd $homeDIR
 
-#madgraph="MG5_aMC_v3.4.2.tar.gz"
-#URL=https://launchpad.net/mg5amcnlo/3.0/3.4.x/+download/$madgraph
-madgraph="MG5_aMC_v3.4.2_fix.tar.gz"
+madgraph="MG5_aMC_v3.5.4.tar.gz"
+URL=https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/$madgraph
+#madgraph="auxFiles/MG5_aMC_v3.4.2_fix.tar.gz"
 echo -n "Install MadGraph (y/n)? "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
 	mkdir MG5;
-	#echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; 
+	echo "[installer] getting MadGraph5"; wget $URL 2>/dev/null || curl -O $URL; 
 	tar -zxf $madgraph -C MG5 --strip-components 1;
 	cd ./MG5/bin;
 	echo "[installer] installing HepMC under MadGraph5"
