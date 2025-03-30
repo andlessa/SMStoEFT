ce1ae3f5f165c1f5104eeb53f2fa4b8e768b9dff
Andre Lessa <lessa.a.p@gmail.com>
2025-03-30 01:06:32 +0100

    Added cards for running FO pp2tt NLO

--- plotting/auxPlots.py
+++ plotting/auxPlots.py
@@ -314,18 +314,21 @@ def getInfo(f,labelsDict=None):
         bannerData = bannerF.read()
     
     # Get process data:
-    processData = bannerData.split('<MGProcCard>')[1].split('</MGProcCard>')[0]
-#     print(processData)
-    # Get model
-    model = processData.split('Begin MODEL')[1].split('End   MODEL')[0]
-    model = model.split('\n')[1].strip()
-    if model in labelsDict:
-        model = labelsDict[model]
-    # Get process
-    proc = processData.split('Begin PROCESS')[1].split('End PROCESS')[0]
-    proc = proc.split('\n')[1].split('#')[0].strip()
-    if proc in labelsDict:
-        proc = labelsDict[proc]
+    if '<MGProcCard>' in bannerData:
+        processData = bannerData.split('<MGProcCard>')[1].split('</MGProcCard>')[0]
+        # Get model
+        model = processData.split('Begin MODEL')[1].split('End   MODEL')[0]
+        model = model.split('\n')[1].strip()
+        if model in labelsDict:
+            model = labelsDict[model]
+        # Get process
+        proc = processData.split('Begin PROCESS')[1].split('End PROCESS')[0]
+        proc = proc.split('\n')[1].split('#')[0].strip()
+        if proc in labelsDict:
+            proc = labelsDict[proc]
+    else:
+        proc = labelsDict['p p > t t~']
+        model = labelsDict['SMS-stop-NLO_SMQCD-UFO']
     
     # Get parameters data:
     parsData = bannerData.split('<slha>')[1].split('</slha>')[0]
@@ -347,9 +350,13 @@ def getInfo(f,labelsDict=None):
 
     
     # Get event data:
-    eventData = bannerData.split('<MGGenerationInfo>')[1].split('</MGGenerationInfo>')[0]
-    nEvents = eval(eventData.split('\n')[1].split(':')[1].strip())
-    xsec = eval(eventData.split('\n')[2].split(':')[1].strip())
+    if '<MGGenerationInfo>' in bannerData:
+        eventData = bannerData.split('<MGGenerationInfo>')[1].split('</MGGenerationInfo>')[0]
+        nEvents = eval(eventData.split('\n')[1].split(':')[1].strip())
+        xsec = eval(eventData.split('\n')[2].split(':')[1].strip())
+    else:
+        nEvents = -1
+        xsec = -1.0
 
     fileInfo = {'model' : model, 'process' : proc, '(mST,mChi,mT,yDM)' : (mST,mChi,mT,yDM),
                'xsec (pb)' : xsec, 'nevents' : nEvents}
