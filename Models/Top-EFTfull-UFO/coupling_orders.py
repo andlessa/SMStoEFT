22d9afd1292e91bfa7ebe0ebd1970e89ee7b9a07
Andre Lessa <lessa.a.p@gmail.com>
2025-05-07 14:07:22 +0200

    Added coupling hierarchies

--- Models/Top-EFTfull-UFO/coupling_orders.py
+++ Models/Top-EFTfull-UFO/coupling_orders.py
@@ -1,22 +1,10 @@
 # This file was automatically created by FeynRules 2.3.49
 # Mathematica version: 14.1.0 for Linux x86 (64-bit) (July 16, 2024)
-# Date: Mon 5 May 2025 18:50:11
+# Date: Wed 7 May 2025 13:32:02
 
 
 from object_library import all_orders, CouplingOrder
 
-
-NP = CouplingOrder(name = 'NP',
-                   expansion_order = 99,
-                   hierarchy = 1)
-
-QCD = CouplingOrder(name = 'QCD',
-                    expansion_order = 99,
-                    hierarchy = 2)
-
-QED = CouplingOrder(name = 'QED',
-                    expansion_order = 99,
-                    hierarchy = 3)
 
 NPcdd = CouplingOrder(name = 'NPcdd',
                       expansion_order = 99,
@@ -65,4 +53,16 @@ NPcuH = CouplingOrder(name = 'NPcuH',
 NPcuu = CouplingOrder(name = 'NPcuu',
                       expansion_order = 99,
                       hierarchy = 1)
+                      
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)                      
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 3)
 
